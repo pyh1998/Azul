d51d739daa5b261ba309551190bc1a5f7a164597
Yuhui Pang <u7211790@anu.edu.au>
2021-03-27 15:41:24 +0800

    Update Bag class

--- src/comp1110/ass2/shareState/Bag.java
+++ src/comp1110/ass2/shareState/Bag.java
@@ -4,14 +4,54 @@ import comp1110.ass2.Tile.Tile;
 
 
 public class Bag {
-    /* The maximum size of tiles that the bag can hold */
-    public static final int MAX_SIZE = 100;
     /**
-     * The array of tiles inside the bag.
+     * Field MAX_SIZE: The maximum size of tiles that the bag can hold
+     * Field tiles: The array of tiles inside the bag.
+     * Field isempty: judge if the Bag is empty.
      */
-    public Tile[] tiles = new Tile[MAX_SIZE];
+    public static final int MAX_SIZE = 100;
+    public Tile[] tiles;
+    public boolean isempty;
 
+    /**
+     * Constructor of Bag
+     * Initialize the boolean isempty in Bag
+     * Initialize the tiles in Bag
+     *
+     */
     public Bag () {
+        isempty = false;
+        tiles = new Tile[MAX_SIZE];
+    }
 
+    /**
+     *Get field the tile array of Bag
+     *
+     */
+    public Tile[] getTiles() {
+        return tiles;
+    }
+
+    /**
+     * Add tiles to Factory from Bag
+     * Update the array of tiles
+     *
+     * @param factory_number the number of factory in the game
+     * @return the array of tiles need to add to Factory
+     */
+    public Tile[] addtoFactory(int factory_number){
+        //TODO FIX the function of addtoFactory
+        return null;
+    }
+
+    /**
+     * Add tiles to Factory from Bag
+     * Update the array of tiles in Bag
+     *
+     * @return the array of tiles from Discard
+     */
+    public Tile[] getTilesfromDiscard(){
+        //TODO FIX the function of getTilesfromDiscard
+        return null;
     }
 }
