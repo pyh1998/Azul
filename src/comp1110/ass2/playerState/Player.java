3ae3fd31953354fe6656ca48e1cd10343563c371
Jiawen Wang <u7111608@anu.edu.au>
2021-03-27 15:28:34 +0800

    updated Player class

--- src/comp1110/ass2/playerState/Player.java
+++ src/comp1110/ass2/playerState/Player.java
@@ -1,15 +1,28 @@
 package comp1110.ass2.playerState;
 
 public class Player {
-    public int score = 0;
-    char id;
+    public enum Id {
+        Player1, PLayer2, Player3, Player4;
+    }
 
-    public Player(char id) {
+    public int score = 0;
+    Id id;
+
+    /**
+     * Constructor of the Player
+     * @param id the id of the player
+     */
+    public Player(Id id) {
         this.id = id;
     }
 
-}
-
-enum Id {
-    Player1, PLayer2, Player3, Player4;
+    /**
+     * Determine whether the player is the first player
+     * @param player the player
+     * @return whether the player is the first player
+     */
+    public boolean isFirstPlayer(Player player) {
+        // TODO FIX the function of isFirstPlayer
+        return false;
+    }
 }
