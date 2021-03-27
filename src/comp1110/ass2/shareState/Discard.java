e209394f35cff7e6926f345137a26fc8bc747059
Yuhui Pang <u7211790@anu.edu.au>
2021-03-27 15:07:20 +0800

    Update Discard class

--- src/comp1110/ass2/shareState/Discard.java
+++ src/comp1110/ass2/shareState/Discard.java
@@ -3,11 +3,60 @@ package comp1110.ass2.shareState;
 import comp1110.ass2.Tile.Tile;
 
 public class Discard {
-    int number = 0;
+
+    /**
+     *Field number: the number of tiles of the Discard
+     *Field tiles: the array of tiles in Discard
+     */
+    private int number;
     public Tile[] tiles;
 
-    public Discard(int number, Tile[] tiles) {
-        this.number = number;
-        this.tiles = tiles;
+    /**
+     * Constructor of Factory
+     * Initialize the tiles number of Discard
+     * Initialize the tiles in Discard
+     *
+     */
+    public Discard() {
+        this.number = 0;
+        this.tiles = new Tile[number];
     }
+
+    /**
+     *Get private field number of Discard
+     *
+     */
+    public int getNumber() {
+        return number;
+    }
+    /**
+     *Get field the tile array of Discard
+     *
+     */
+    public Tile[] getTiles() {
+        return tiles;
+    }
+
+    /**
+     * Add tiles to Discard from Storage and Floor
+     * Update the array of tiles
+     *
+     * @param tiles the array of tiles from other places
+     */
+    public void addTiles(Tile[] tiles){
+        this.number += tiles.length;
+        //TODO FIX the function of addTiles
+    }
+    /**
+     * Move all tiles to Bag when Bag has no tile
+     * Make the number of Discard equals zero and empty the array of tiles
+     *
+     */
+    public void movetoBag(){
+        this.number = 0;
+        //TODO FIX the function of movetoBag
+    }
+
+
+
 }
