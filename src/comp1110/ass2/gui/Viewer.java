da0b3221cab4ed041b9016a4997b96640ac4393e
Yuhui Pang <u7211790@anu.edu.au>
2021-04-16 10:03:18 +0800

    Some attempts with TASK4

--- src/comp1110/ass2/gui/Viewer.java
+++ src/comp1110/ass2/gui/Viewer.java
@@ -1,5 +1,12 @@
 package comp1110.ass2.gui;
 
+import comp1110.ass2.Tile.Tile;
+import comp1110.ass2.play.PlayerState;
+import comp1110.ass2.play.SharedState;
+import comp1110.ass2.playerState.Floor;
+import comp1110.ass2.playerState.Mosaic;
+import comp1110.ass2.playerState.Player;
+import comp1110.ass2.playerState.Storage;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -9,6 +16,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class Viewer extends Application {
@@ -21,6 +31,97 @@ public class Viewer extends Application {
     private TextField playerTextField;
     private TextField boardTextField;
 
+    public Group drawStorage(String storageStr){
+        Storage storage = new Storage(storageStr);
+        Tile[] tileType = storage.getTileType();
+        int[] tileNum = storage.getTileNumber();
+        int side = 40;
+        int space = 5;
+        Group storageGroup = new Group();
+
+        for(int i=0;i<Storage.NUMBER_ROWS;i++){
+            int count = 0;
+            for(int j=0;j<=i;j++){
+                Rectangle r = new Rectangle();
+                r.setX((Storage.NUMBER_ROWS - j) * (side + space));
+                r.setY(i * (side + space));
+                r.setWidth(side);
+                r.setHeight(side);
+                if(tileType[i] == null || count == tileNum[i]){
+                    r.setFill(Color.GRAY);
+                }
+                else{
+                    r.setFill(tileType[i].getTILE_COLOR());
+                    count++;
+                }
+                storageGroup.getChildren().add(r);
+            }
+        }
+
+        return storageGroup;
+    }
+
+    public Group drawMosaic(String mosaicStr){
+        Mosaic mosaic = new Mosaic(mosaicStr);
+        Tile[][] tiles = mosaic.getTiles();
+
+        int side = 40;
+        int space = 5;
+        Group mosaicGroup = new Group();
+
+        for(int i=0;i<Mosaic.HEIGHT;i++){
+            for(int j=0;j<Mosaic.HEIGHT;j++){
+                Rectangle r = new Rectangle();
+                r.setX(j * (side + space));
+                r.setY(i * (side + space));
+                r.setWidth(side);
+                r.setHeight(side);
+                if(tiles[i][j] == null){
+                    r.setFill(Color.GRAY);
+                }
+                else{
+                    r.setFill(tiles[i][j].getTILE_COLOR());
+                }
+                mosaicGroup.getChildren().add(r);
+            }
+        }
+
+        return mosaicGroup;
+    }
+
+    public Group drawPlayer(String playerStr){
+        Player player = new Player(playerStr);
+        Group playerGroup = new Group();
+        char playerId = player.getId();
+        int score = player.getScore();
+        Label labelI = new Label(String.valueOf("Player: " + playerId));
+        Label labelS = new Label(String.valueOf("Score: " + score));
+        labelS.setLayoutX(100);
+        playerGroup.getChildren().addAll(labelI,labelS);
+        return playerGroup;
+    }
+
+    public Group drawFloor(String floorStr){
+        Floor floor = new Floor(floorStr);
+        Tile[] tiles =floor.getTiles();
+        Group floorGroup = new Group();
+        int side = 40;
+        int space = 5;
+        for(int i=0;i<Floor.MAX_LENGTH;i++){
+            Rectangle r = new Rectangle();
+            r.setX(i * (side + space));
+            r.setWidth(side);
+            r.setHeight(side);
+            if(tiles[i] == null){
+                r.setFill(Color.GRAY);
+            }
+            else{
+                r.setFill(tiles[i].getTILE_COLOR());
+            }
+            floorGroup.getChildren().add(r);
+        }
+        return floorGroup;
+    }
 
     /**
      * Draw a placement in the window, removing any previously drawn placements
@@ -28,7 +129,48 @@ public class Viewer extends Application {
      * @param state an array of two strings, representing the current game state
      *              TASK 4
      */
+
+
+
     void displayState(String[] state) {
+        String playerStateStr = state[0];
+        //A31Mb01d03e04e10c13d14d20a22b23c24e32a33b34e43S4d2FB10Ma00b01e10b12d14d20e21c30c41a44S2c23d3F
+        int player_cnt = 0;
+        for (int i = 'A'; i < 'E'; i++) {
+            if (playerStateStr.indexOf((char) i) != -1)
+                player_cnt++;
+            }
+
+        int currentPlayerId = 0;
+        for (int i = 0; i < player_cnt; i++) {
+            if (i + 1 < player_cnt){
+                playerStateStr = state[0].substring(playerStateStr.indexOf((char) (i + 'A')),playerStateStr.indexOf((char) (i + 'B')));
+            }
+            else{
+                playerStateStr = state[0].substring(state[0].indexOf((char) (i + 'A')));
+            }
+
+            PlayerState playerState = new PlayerState(playerStateStr);
+            Group player = drawPlayer(playerState.getPlayer().getStateStr());
+            Group storage = drawStorage(playerState.getStorage().getStateStr());
+            Group mosaic = drawMosaic(playerState.getMosaic().getStateStr());
+            Group floor = drawFloor(playerState.getFloor().getStateStr());
+
+            storage.setLayoutX(currentPlayerId * 600);
+            storage.setLayoutY(200);
+            mosaic.setLayoutX(300 + currentPlayerId * 600);
+            mosaic.setLayoutY(200);
+            player.setLayoutX(45 + currentPlayerId * 600);
+            player.setLayoutY(150);
+            floor.setLayoutX(45 + currentPlayerId * 600);
+            floor.setLayoutY(500);
+
+            controls.getChildren().addAll(player,storage,mosaic,floor);
+            currentPlayerId ++;
+        }
+
+
+
         // FIXME Task 4: implement the simple state viewer
     }
 
